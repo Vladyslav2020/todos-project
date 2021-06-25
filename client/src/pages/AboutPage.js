1937eae7762fa504f2fcaae6eb752edbbd2c13af
Vladyslav2020 <vmskl2020@gmail.com>
2021-06-25 22:38:27 +0300

    Change NodeJS in AboutPage to Java Spring Framework

--- client/src/pages/AboutPage.js
+++ client/src/pages/AboutPage.js
@@ -7,9 +7,9 @@ const AboutPage = () => {
             <div className="card text-center" style={{width: '25rem'}}>
                 <div className="card-body">
                     <h3 className="card-title">About project</h3>
-                    <h5 className="mb-3"><p className="card-text">This is a simple app for todos. 
-                    <br/>This application uses the following technologies: <strong>React JS</strong>,&nbsp;
-                    <strong>Mongodb</strong>, <strong>Node JS</strong>, <strong>Express</strong>.</p></h5>
+                    <h5 className="mb-3"><p className="card-text">This is a simple app for todos.
+                    <br/>This application uses the following technologies: <strong>React JS</strong>, <strong>Redux</strong>,&nbsp;
+                    <strong>MySQL</strong>, <strong>Java Spring Framework</strong>.</p></h5>
                     <NavLink className="btn btn-primary" to='/todos'>Go home</NavLink>
                 </div>
             </div>
